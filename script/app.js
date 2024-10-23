6cb9c44f613f0f3d42b42b510f900f9a3cce4533
stevenjake72898 <fajarillostevenjake@gmail.com>
2024-10-24 01:35:18 +0800

    Admin changes

--- script/app.js
+++ script/app.js
@@ -90,6 +90,7 @@ onValue(connectedRef, (snapshot) => {
 });
 
 $(document).ready(() => {
+  let lists2 = ``;
   let isAllowedToAppend = true;
 
   if (
@@ -97,13 +98,14 @@ $(document).ready(() => {
     sessionStorage.getItem("uid") != null
   ) {
     // window.location.href = "admin.html";
-  }
-   else {
+  } else {
     // if(window.location.href.split("/")[3] != "index.html"){
-    if(window.location.href.includes("index.html")){
+    if (window.location.href.includes("index.html")) {
       window.location.href = "index.html";
     }
   }
+
+  $("#date-today").text(getCurrentDate("mdyt") + " Bookings");
 
   const connectedRef = ref(database, "users/");
   onValue(connectedRef, (snapshot) => {
@@ -125,23 +127,197 @@ $(document).ready(() => {
       let d = data.drivers;
       for (let driver in d) {
         lists += `<tr>`;
+        // lists2 += `<tr id="${d[driver].driverid.trim()}">`;
+
         lists += `<td>${d[driver].plateno}</td>`;
+        // lists2 += `<td col="plateno">${d[driver].plateno}</td>`;
+
         lists += `<td>${d[driver].drivername}</td>`;
+        // lists2 += `<td col="drivername">${d[driver].drivername}</td>`;
+
+        lists += `<td>${d[driver].driveremail}</td>`;
+        // lists2 += `<td col="email">${d[driver].email}</td>`;
+
+        // lists2 += `<td col="password">${d[driver].password}</td>`;
+
         lists += `<td>${d[driver].driverage}</td>`;
+        // lists2 += `<td col="driverage">${d[driver].driverage}</td>`;
+
         lists += `<td>${d[driver].driveraddress}</td>`;
+        // lists2 += `<td col="driveraddress">${d[driver].driveraddress}</td>`;
+
         lists += `<td>${d[driver].drivercontact}</td>`;
+        // lists2 += `<td col="drivercontact">${d[driver].drivercontact}</td>`;
+
         lists += `<td>${d[driver].vanmodel}</td>`;
+        // lists2 += `<td col="vanmodel">${d[driver].vanmodel}</td>`;
+
         lists += `<td>${d[driver].dateaccountregistered}</td>`;
+        // lists2 += `<td col="dateaccountregistered">${d[driver].dateaccountregistered}</td>`;
+
         lists += `</tr>`;
+        // lists2 += `</tr>`;
+
+        lists2 += `
+           <tr style="align-items: center;" driverid="${d[driver].driverid.trim()}">
+            <td>
+              <input type="text" class="new-row-input" id="plate-no" placeholder="Van Plate Number" value="${d[driver].plateno}" autocomplete="off"/>
+            </td>
+            <td>
+              <input type="text" class="new-row-input" id="driver-name" placeholder="Driver Name" value="${d[driver].drivername}" autocomplete="off"/>
+            </td>
+            <td>
+              <input type="email" class="new-row-input" id="driver-email" placeholder="Email" value="${d[driver].driveremail}" autocomplete="off"/>
+            </td>
+            <td>
+              <input type="password" class="new-row-input" id="driver-password" placeholder="Password" value="${d[driver].driverpassword}" autocomplete="off"/>
+            </td>
+            <td>
+              <input type="text" class="new-row-input" id="driver-age" placeholder="Driver Age" value="${d[driver].driverage}" autocomplete="off"/>
+            </td>
+            <td>
+              <input type="text" class="new-row-input" id="driver-address" placeholder="Driver Address" value="${d[driver].driveraddress}" autocomplete="off"/>
+            </td>
+            <td>
+              <input type="text" class="new-row-input" id="driver-contact" placeholder="Driver Contact No." value="${d[driver].drivercontact}" autocomplete="off"/>
+            </td>
+            <td>
+              <input type="text" class="new-row-input" id="van-model" placeholder="Van Model" value="${d[driver].vanmodel}" autocomplete="off"/>
+            </td>
+            <td>
+              <input type="date" class="new-row-input" id="date-account-registered" placeholder="Account Registered" value="${d[driver].dateaccountregistered}" disabled autocomplete="off"/>
+            </td>
+          </tr>
+        `;
       }
-      
-      $("#btn-add-new").before(lists);
+
+      $("#list-of-drivers").html(lists);
       // $("#btn-add-new").prev().remove();
       // isAllowedToAppend = true;
       // }
     } else {
       console.log("not connected");
     }
+  });
+
+  // On Total Bookings click
+  $("#total-booking").on("click", () => {
+    $("#tbl-total-bookings").DataTable({
+      searching: true,
+      responsive: true,
+      // columnDefs: [
+      //   { visible: false,
+      //     targets: [0] }
+      // ],
+      ordering: true,
+      // processing: false,
+      // serverSide: false,
+      destroy: true,
+      info: false,
+      language: {
+        emptyTable: "No entries to show",
+        infoEmpty: "No entries to show",
+      },
+    });
+  });
+
+  // On UpdateInfo click
+  $("#update-info").on("click", () => {
+    $("#tbl-tbody-update-info").html(lists2);
+
+    $("#tbl-update-info").DataTable({
+      searching: true,
+      responsive: true,
+      dom: "t",
+      // columnDefs: [
+      //   { visible: false,
+      //     targets: [0] }
+      // ],
+      ordering: false,
+      // processing: false,
+      // serverSide: false,
+      destroy: true,
+      info: false,
+      language: {
+        emptyTable: "No entries to show",
+        infoEmpty: "No entries to show",
+      },
+    });
+  });
+
+  // Change Values of Users
+  $("#tbl-update-info").on("click", (e) => {
+
+    if(e.target.nodeName == "INPUT"){
+      let originalValue = $(e.target).val();
+      $(e.target).attr("originalVal", originalValue);
+    }
+
+    // console.log(e.target.nodeName);
+  });
+
+  // Check if value changed
+  $(document).on("blur", "#tbl-update-info input", (e) => {
+    let originalValue = $(e.target).attr("originalVal");
+    let blurredValue = $(e.target).val();
+    originalValue = originalValue == undefined ? "" : originalValue;
+    blurredValue  = blurredValue  == undefined ? "" : blurredValue;
+    
+    if(originalValue != blurredValue){
+      console.log("Need to update to database");
+      let driverid = $(e.target).parent().parent().attr("driverid");
+      let blurrednode = $(e.target).attr("id").replaceAll("-", "");
+      // blurrednode = blurrednode == "driverpassword" ? "password" : blurrednode;
+      // blurrednode = blurrednode == "driveremail"    ? "email"    : blurrednode;
+      
+      const updates = {};
+      updates[`/users/drivers/${driverid}/${blurrednode}`] = blurredValue;
+      console.log(updates);
+      update(ref(database), updates)
+        .then(() => {
+          iziToast.success({
+            title: "Successfully Saved",
+            message: `${blurrednode.toUpperCase()} has been saved`,
+            icon: "fa fa-save",
+            position: "topRight",
+            timeout: 3000,
+          });
+        })
+        .catch((error) => {
+          iziToast.warning({
+            title: "Save Failed",
+            message: `${error}`,
+            icon: "fa fa-bell-exclamation",
+            position: "topRight",
+            timeout: 4000,
+          });
+        });
+      
+        console.log(driverid, blurrednode);
+    } else {  
+      console.log("Same Value");
+    }
+  });
+
+  // On Total Bookings click
+  $("#total-booking").on("click", () => {
+    $("#tbl-total-bookings").DataTable({
+      searching: true,
+      responsive: true,
+      // columnDefs: [
+      //   { visible: false,
+      //     targets: [0] }
+      // ],
+      ordering: true,
+      // processing: false,
+      // serverSide: false,
+      destroy: true,
+      info: false,
+      language: {
+        emptyTable: "No entries to show",
+        infoEmpty: "No entries to show",
+      },
+    });
   });
 
   function loginUser(email, password) {
@@ -218,6 +394,12 @@ $(document).ready(() => {
         <input type="text" class="new-row-input" id="driver-name" placeholder="Driver Name" autocomplete="off"/>
       </td>
       <td>
+        <input type="email" class="new-row-input" id="driver-email" placeholder="Email" autocomplete="off"/>
+      </td>
+      <td>
+        <input type="password" class="new-row-input" id="driver-password" placeholder="Password" autocomplete="off"/>
+      </td>
+      <td>
         <input type="text" class="new-row-input" id="driver-age" placeholder="Driver Age" autocomplete="off"/>
       </td>
       <td>
@@ -288,6 +470,20 @@ $(document).ready(() => {
           isAllowedToAppend = false;
 
           console.log(data);
+          // Add additionla data
+          let nameAbbrevation = data["drivername"].trim().substring(0, 2);
+          let dateAbbrevation = data["dateaccountregistered"]
+            .trim()
+            .replaceAll("-", "");
+          let plateAbbrevation = data["plateno"].trim().replaceAll("-", "");
+
+          data["driverid"] = (
+            nameAbbrevation +
+            dateAbbrevation +
+            plateAbbrevation
+          ).toUpperCase();
+          data["isAvailable"] = false;
+
           // Save to database
           const updates = {};
           updates[`/users/drivers/${data["drivername"]}`] = data;
@@ -303,7 +499,7 @@ $(document).ready(() => {
               });
 
               // setTimeout(() => {
-                window.location.href = window.location.href;
+              window.location.href = window.location.href;
               // }, 1500);
             })
             .catch((error) => {
@@ -354,6 +550,20 @@ $(document).ready(() => {
     let day = String(today.getDate()).padStart(2, "0");
     let month = String(today.getMonth() + 1).padStart(2, "0"); // Months are zero-based
     let year = today.getFullYear();
+    let monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
 
     if (format == "dmy") {
       return `${day}/${month}/${year}`;
@@ -361,39 +571,45 @@ $(document).ready(() => {
       return `${year}-${month}-${day}`;
     } else if (format == "ymd/") {
       return `${year}/${month}/${day}`;
+    } else if (format == "mdyt") {
+      return `${monthNames[today.getMonth()]} ${day}, ${year}`;
     }
   }
 
-  
   $("#btn-sign-out").on("click", () => {
     iziToast.question({
       // timeout: 20000,
       close: false,
       overlay: true,
       // displayMode: 'once',
-      id: 'question',
+      id: "question",
       zindex: 999,
-      title: 'Logout',
-      message: 'Are you sure you want to logout?',
-      position: 'center',
+      title: "Logout",
+      message: "Are you sure you want to logout?",
+      position: "center",
       buttons: [
-          ['<button><b>YES</b></button>', function (instance, toast) {
-              instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-              window.location.href = "index.html";
-              sessionStorage.setItem("uid", "");
-          }, true],
-          ['<button>NO</button>', function (instance, toast) {
-   
-              instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-   
-          }],
+        [
+          "<button><b>YES</b></button>",
+          function (instance, toast) {
+            instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+            window.location.href = "index.html";
+            sessionStorage.setItem("uid", "");
+          },
+          true,
+        ],
+        [
+          "<button>NO</button>",
+          function (instance, toast) {
+            instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+          },
+        ],
       ],
-      onClosing: function(instance, toast, closedBy){
-          console.info('Closing | closedBy: ' + closedBy);
+      onClosing: function (instance, toast, closedBy) {
+        console.info("Closing | closedBy: " + closedBy);
       },
-      onClosed: function(instance, toast, closedBy){
-          console.info('Closed | closedBy: ' + closedBy);
-      }
-  });
+      onClosed: function (instance, toast, closedBy) {
+        console.info("Closed | closedBy: " + closedBy);
+      },
+    });
   });
 });
